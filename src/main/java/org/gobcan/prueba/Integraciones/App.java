12fe651f1f9e691bac6f3aa207024da5c172f863
cumpierrez <cumpierrez@gmail.com>
2021-05-24 10:02:11 +0100

    v2

--- src/main/java/org/gobcan/prueba/Integraciones/App.java
+++ src/main/java/org/gobcan/prueba/Integraciones/App.java
@@ -1,13 +1,36 @@
 package org.gobcan.prueba.Integraciones;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+	public final static Logger logger = Logger.getLogger("org.gobcan.prueba.testprueba.App");
+	
+	public static int suma(int a, int b) 
+	{
+        if ((a == 0) && (b ==0)) {
+            return -1; 
+        }
+        else
+            return a+b;
+	}
+	
+	public Integer suma2(Integer numero1, Integer numero2)
+	{
+		//numero2 = null;
+		return numero1.intValue() + numero2.intValue();
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	logger.log(Level.INFO, "Hello World!" );
+    	logger.log(Level.INFO, "La suma del 10 con el 35 es {0}", suma(10,35) );
     }
+    
 }
--- src/test/java/org/gobcan/prueba/Integraciones/AppTest.java
+++ src/test/java/org/gobcan/prueba/Integraciones/AppTest.java
@@ -1,7 +1,10 @@
 package org.gobcan.prueba.Integraciones;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.gobcan.prueba.Integraciones.App;
 import org.junit.Test;
 
 /**
@@ -17,4 +20,31 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    
+    @Test
+    public void pruebaDeSumaTest()
+    {
+        assertEquals("La suma ha dejado de ser correcta", 20, App.suma(10, 10));
+    }
+    
+    @Test
+    public void pruebaDeSumaTest2()
+    {
+        assertNotEquals("La suma ha dejado de ser correcta", 10000, App.suma(100, 100));
+    	assertEquals("La suma ha dejado de ser correcta", 30, App.suma(20, 10) );
+    	
+    }
+    
+    @Test
+    public void pruebaDeSumaTest3()
+    {
+        assertEquals("La suma ha dejado de ser correcta", 4, App.suma(2, 2));
+    }
+    
+    @Test
+    public void pruebaDeSumaTest4()
+    {
+        assertEquals("La suma ha dejado de ser correcta", -1, App.suma(0, 0));
+    }    
 }
